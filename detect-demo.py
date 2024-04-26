6a07fb20ca027add579af8e52a3cc5f0f3458b3c
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-26 13:56:24 +0000

    changes to code

--- detect-demo.py
+++ detect-demo.py
@@ -3,4 +3,10 @@ import pandas as pd
 from io import StringIO
 
 observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg','png'])
-st.image(observe_photo, caption='Uploaded photo for PPE detection')
+
+try:
+    st.image(observe_photo, caption='Uploaded photo for PPE detection')
+except AttributeError:
+    st.header('Please upload an image first...')
+else:
+    st.image(observe_photo, caption='Uploaded photo for PPE detection')
