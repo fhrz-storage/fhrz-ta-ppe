c0c95cce86965ad9d4dbdf8ac5c866fc492a0cbd
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-26 15:11:23 +0000

    "add functionality"

--- detect-demo.py
+++ detect-demo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import ultralytics
 
 observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg','png'])
 
@@ -10,3 +11,10 @@ except AttributeError:
     st.header('Please upload an image first...')
 # else:
 #     st.image(observe_photo, caption='Uploaded photo for PPE detection')
+
+if st.button("Detect PPEs", type="primary"):
+    detect = YOLO('/content/runs/detect/train2/weights/best.pt')
+    try:
+        results = detect.predict(source=observe_photo, save=True) # Display preds. Accepts all YOLO predict arguments
+    except AttributeError:
+        st.text("Please enter the photos that you want to upload first")
