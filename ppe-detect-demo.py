3a386bb1accc1b101e955f06912c9ee1b199bc2a
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-26 20:13:46 +0700

    Delete ppe-detect-demo.py

--- ppe-detect-demo.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-from io import StringIO
-
-observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg','png'])
-st.image(observe_photo, caption='Uploaded photo for PPE detection')
