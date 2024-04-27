6742c0e22c8fc16cb18a097eac93a92aae2f5bfc
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-27 10:47:41 +0000

    add output format

--- detect-demo.py
+++ detect-demo.py
@@ -10,25 +10,17 @@ observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpeg
 
 if observe_photo is not None:
     try:
-        image = Image.open(observe_photo)
-        image_detect = st.image(image, caption='Uploaded photo for PPE detection')
+        image_detect = st.image(observe_photo, caption='Uploaded photo for PPE detection', output_format='PNG')
     except AttributeError:
         st.header('Please upload an image first...')
 
 # Detecting using our trained model
 
 if st.button("Detect PPEs", type="primary"):
-    if observe_photo is not None:
-        file_extension = observe_photo.name.split('.')[-1].lower()
-        if file_extension in ['jpg', 'jpeg', 'png']:
-            detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
-            try:
-                with st.spinner("Detecting objects..."):
-                    results = detect.predict(observe_photo, save=True)
-                    image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
-            except AttributeError:
-                st.text("Error: Unable to detect PPEs.")
-        else:
-            st.text("Error: Please upload an image in JPEG or PNG format.")
-    else:
-        st.text("Error: Please upload an image first.")
+    detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
+    try:
+        with st.spinner("Detecting objects..."):
+            results = detect.predict(observe_photo, save=True)
+            image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
+    except AttributeError:
+        st.text("Error: Unable to detect PPEs.")
