3b0bbf0a20c6526d6e998aa8d9ab15b474959589
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-27 10:56:48 +0000

    adding codes

--- detect-demo.py
+++ detect-demo.py
@@ -10,7 +10,8 @@ observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpeg
 
 if observe_photo is not None:
     try:
-        image_detect = st.image(observe_photo, caption='Uploaded photo for PPE detection', output_format='PNG')
+        image_detect = observe_photo.read()
+        st.image(image_detect, caption='Uploaded photo for PPE detection', output_format='PNG')
     except AttributeError:
         st.header('Please upload an image first...')
 
@@ -20,7 +21,8 @@ if st.button("Detect PPEs", type="primary"):
     detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
     try:
         with st.spinner("Detecting objects..."):
-            results = detect.predict(observe_photo, save=True)
+            results = detect.predict(image_detect, save=True)
             image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
+        st.image(results, output_format='PNG')
     except AttributeError:
         st.text("Error: Unable to detect PPEs.")
