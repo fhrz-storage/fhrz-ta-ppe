5ea9b90e6dcb45c06be6fff84eedebe9aa94a0bd
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-27 08:12:51 +0000

    deleting unnecessary files, changing permalink blob to raw link

--- detect-demo.py
+++ detect-demo.py
@@ -15,9 +15,11 @@ except AttributeError:
 # Detecting using our trained model
 
 if st.button("Detect PPEs", type="primary"):
-    detect = YOLO('https://github.com/fhrz-storage/fhrz-ta-ppe/blob/a5dee7a4850b25a70d045f6ab28719355db4d68e/peripherals/weights/best.pt')
+    detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
     try:
         results = detect.predict(source=observe_photo, show=True)
         image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
     except AttributeError:
         st.text("Please enter the photos that you want to upload first")
+
+        
