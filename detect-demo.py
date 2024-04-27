8c2526b83ccf330953762ee12ff3612e712b14d8
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-27 07:59:31 +0000

    add trained weights

--- detect-demo.py
+++ detect-demo.py
@@ -15,7 +15,7 @@ except AttributeError:
 # Detecting using our trained model
 
 if st.button("Detect PPEs", type="primary"):
-    detect = YOLO('https://github.com/fhrz-storage/fhrz-ta-ppe/blob/a5dee7a4850b25a70d045f6ab28719355db4d68e/peripherals/best.pt')
+    detect = YOLO('https://github.com/fhrz-storage/fhrz-ta-ppe/blob/a5dee7a4850b25a70d045f6ab28719355db4d68e/peripherals/weights/best.pt')
     try:
         results = detect.predict(source=observe_photo, show=True)
         image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
