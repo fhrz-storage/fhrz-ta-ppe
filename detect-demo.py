3c8d9d6137474f49abe7de36886a83c339391fc5
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-27 11:05:08 +0000

    adding io reader

--- detect-demo.py
+++ detect-demo.py
@@ -1,17 +1,19 @@
 import streamlit as st
 from PIL import Image
 from ultralytics import YOLO
+import io
 
 # Upload the photo that we want to observe
 
-observe_photo = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpeg', 'png'])
+uploaded_image = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpeg', 'png'])
 
 # Check whether the photo is correct
 
-if observe_photo is not None:
+if uploaded_image is not None:
     try:
-        image_detect = observe_photo.read()
-        st.image(image_detect, caption='Uploaded photo for PPE detection', output_format='PNG')
+        image_raw = uploaded_image.get_value()
+        image_usable = Image.open(io.BytesIO(image_raw))
+        st.image(image_usable, caption='Uploaded photo for PPE detection', output_format='PNG')
     except AttributeError:
         st.header('Please upload an image first...')
 
@@ -21,8 +23,8 @@ if st.button("Detect PPEs", type="primary"):
     detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
     try:
         with st.spinner("Detecting objects..."):
-            results = detect.predict(image_detect, save=True)
-            image_detect = st.image(results) # Display preds. Accepts all YOLO predict arguments
+            results = detect.predict(image_usable, save=True)
+            st.image(results) # Display preds. Accepts all YOLO predict arguments
         st.image(results, output_format='PNG')
     except AttributeError:
         st.text("Error: Unable to detect PPEs.")
