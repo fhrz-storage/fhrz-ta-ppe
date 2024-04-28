42ea4c3c4ad8c8f467a407c2d0e1560a84b9df48
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-28 06:59:20 +0000

    tweaks

--- detect-demo.py
+++ detect-demo.py
@@ -13,14 +13,14 @@ if uploaded_image is not None:
     # try:
     image_raw = uploaded_image.read()
     image_usable = Image.open(io.BytesIO(image_raw))
-    st.image(image_usable, caption='Uploaded photo for PPE detection')
+    st.image(image_usable, caption=f'{uploaded_image.name}')
 
     # Detecting using our trained model
 
     if st.button("Detect PPEs", type="primary"):
         detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
         with st.spinner("Detecting objects..."):
-            results = detect.predict(f"{uploaded_image.name}.png")
+            results = detect.predict(f"{uploaded_image.name}")
             for x in results:
                 st.image(x, caption="Image with object detected in it")
 
