3ceacb64300740d5d12118e6a4ebb739877adc2e
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-28 07:18:48 +0000

    trials

--- detect-demo.py
+++ detect-demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from ultralytics import YOLO
 import io
-import tempfile
+import cv2
 
 
 # Upload the photo that we want to observe
@@ -15,19 +15,16 @@ uploaded_image = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpe
 if uploaded_image is not None:
     # try:
     image_raw = uploaded_image.read()
-    image_usable = Image.open(io.BytesIO(image_raw))
-    st.image(image_usable, caption=f'{uploaded_image.name}')
+    image_repackaged = Image.open(io.BytesIO(image_raw))
+    image_usable = image_repackaged.save("materials.png")
+    st.image(image_usable)
 
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(image_raw)
-        f.flush()
-        st.write(f.name)
     # Detecting using our trained model
 
     if st.button("Detect PPEs", type="primary"):
         detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
         with st.spinner("Detecting objects..."):
-            results = detect.predict(f"{f.name}")
+            results = detect.predict(f"image_usable")
             for x in results:
                 st.image(x, caption="Image with object detected in it")
 
