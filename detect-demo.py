f401c186530c8703aba0774c03c1a5878ef98180
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-28 06:51:49 +0000

    changes to bytes processing

--- detect-demo.py
+++ detect-demo.py
@@ -13,7 +13,8 @@ uploaded_image = st.file_uploader("Upload a photo to observe", type=['jpg', 'jpe
 if uploaded_image is not None:
     # try:
     image_raw = uploaded_image.read()
-    image_usable = Image.open(image_raw)
+    image_bytes = io.BytesIO(image_raw)
+    image_usable = Image.open(image_bytes)
     st.image(image_usable, caption='Uploaded photo for PPE detection')
 
     # Detecting using our trained model
