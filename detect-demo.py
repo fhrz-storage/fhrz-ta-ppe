5806b78ace479b608a1067c9f37909735c1b331c
fhrz-storage <168087342+fhrz-storage@users.noreply.github.com>
2024-04-28 09:45:13 +0000

    s"

--- detect-demo.py
+++ detect-demo.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
 
     detect = YOLO('https://raw.githubusercontent.com/fhrz-storage/fhrz-ta-ppe/main/peripherals/weights/best.pt')
     # with st.spinner("Detecting objects..."):
-    results = detect.predict(pil_image, save=True)
+    results = detect.predict(pil_image, save=True, save_dir=f"runs/detect/predict9/{uploaded_file.name}")
     # st.text(results)
     pil_result = Image.open(f"runs/detect/predict9/{uploaded_file.name}")
     st.image(pil_result)
